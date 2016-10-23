607d438a28c6153ab640a2a1e0b2a99960e69b76
King-Yan Kwan <kykwan@linux.suse>
2016-10-23 11:38:04 +0800

    Provide target without implementation header

--- exec_target.mk
+++ exec_target.mk
@@ -1,2 +1,14 @@
-%: %.cpp 
+%_test: %_test.cpp %.hpp
 	$(CC) $(LANG_VER) $(DIAG_FLAGS) -o $@ $@.cpp
+
+%_test: %_test.cpp %.hpp %.ipp
+	$(CC) $(LANG_VER) $(DIAG_FLAGS) -o $@ $@.cpp
+
+%: %.cpp %.hpp
+	$(CC) $(LANG_VER) $(DIAG_FLAGS) -o $@ $@.cpp
+
+%: %.cpp
+	$(CC) $(LANG_VER) $(DIAG_FLAGS) -o $@ $@.cpp
+
+%: %.o
+	$(CC) $(LANG_VER) $(DIAG_FLAGS) -o $@ $@.o
